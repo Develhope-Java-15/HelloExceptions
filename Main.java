45b9a6db4ce49e0e1384190e09b57a982b1ee93a
Giuseppe <tasso96@gmail.com>
2023-08-29 15:03:11 +0200

    aggiunta arithmetic exception

--- Main.java
+++ Main.java
@@ -1,16 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            int result = power(5, -2);
+            int result = power(0, 0);
             System.out.println(result);
         } catch(PowerException e) {
             System.err.println("Unable to execute power operation!");
+        } catch (ArithmeticException e) {
+            System.err.println("Errore!");
         }
+        
+        System.out.println("All exception have been ended");
     }
 
     private static int power(int base, int exponent) throws PowerException {
         if(exponent < 0) {
             throw new PowerException(base == 0);
+        }
+        if(exponent == 0 && base == 0) {
+            throw new ArithmeticException("Base and exponent equals 0");
         }
 
         int result = 1;
